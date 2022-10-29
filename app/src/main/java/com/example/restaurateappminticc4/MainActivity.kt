0acede60ac34ee1552f90913cdadf9e9f86c6a94
Heler Paez <heler2091@gmail.com>
2022-10-28 22:51:01 -0500

    agreado boton de volver atras y barra de navegacion

--- app/src/main/java/com/example/restaurateappminticc4/MainActivity.kt
+++ app/src/main/java/com/example/restaurateappminticc4/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.example.restaurateappminticc4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.barra))
+
+        val floating: View = findViewById(R.id.fabatras)
+
+        floating.setOnClickListener{ view->
+            val ingreso = Intent( this,logActivity::class.java )
+            startActivity(ingreso)
+            //Snackbar.make(view,  "configuracion de la snack bar", Snackbar.LENGTH_SHORT).show()
+        }
+
+
+
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_barra, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when(item.itemId) {
+        R.id.itmbuscar -> {
+            Toast.makeText(this, "Selecion de buscar", Toast.LENGTH_SHORT).show()
+            true
+        }
+
+        R.id.itmubicacion -> {
+            Toast.makeText(this, "Selecion de Ubicacion", Toast.LENGTH_SHORT).show()
+            true
+        }
+
+        else-> {
+            super.onOptionsItemSelected(item)
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/example/restaurateappminticc4/logActivity.kt
+++ app/src/main/java/com/example/restaurateappminticc4/logActivity.kt
@@ -1,19 +1,78 @@
 package com.example.restaurateappminticc4
 
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 
 class logActivity : AppCompatActivity() {
+    private var etUsuario: EditText? = null
+    private var etContrasena: EditText? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log)
+        //setSupportActionBar(findViewById(R.id.barra))
+
+        etUsuario = findViewById(R.id.etUsuario)
+        etContrasena = findViewById(R.id.etContrasena)
+
+
     }
 
     fun ingresar(btnlogin: View) {
-        val ingreso = Intent( this,MainActivity::class.java )
-        startActivity(ingreso)
+
+        val usuario: String = etUsuario!!.text.toString()
+        val contra: String = etContrasena!!.text.toString()
+
+        val botonPositivo = {
+            xx:DialogInterface, yy:Int->
+            val ingreso = Intent( this,MainActivity::class.java )
+            startActivity(ingreso)
+        }
+
+        val botonNegativo = {
+            nombre1:DialogInterface, nombre2:Int->
+
+        }
+
+        if ( usuario == "usuariox" && contra == "123" ){
+            val dialog = AlertDialog.Builder(this)
+                .setTitle("Bienvenido " + usuario)
+                .setMessage("Quieres pasar al menú")
+                .setPositiveButton("Confirmar", botonPositivo)
+                .setNegativeButton("Cancelar", botonNegativo)
+                .create().show()
+        }
+        else {
+            val dialogW = AlertDialog.Builder(this)
+                .setTitle(R.string.alert_title)
+                .setMessage(R.string.alert_text)
+                .create().show()
+        }
+
+
+
+
+       // if (etUsuario!!.text.toString()=="usuariox") {
+         //   if (etContrasena!!.text.toString()=="123") {
+           //     val ingreso = Intent( this,MainActivity::class.java )
+             //   startActivity(ingreso)
+           // }
+       // }
+       // else{
+         //   val dialog = AlertDialog.Builder(this)
+           //     .setTitle(R.string.alert_title)
+             //   .setMessage(R.string.alert_text)
+               // .create().show()
+       // }
+
+        //val ingreso = Intent( this,MainActivity::class.java )
+        //startActivity(ingreso)
     }
 
     fun registrar(btnlogin: View) {
